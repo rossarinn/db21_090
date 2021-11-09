247c53ec4bd194c1649a57984c31764cc1b21290
rossarinn <rossarin.n@ku.th>
2021-11-10 00:41:18 +0700

    Update updatefrom.php

--- views/Person/updatefrom.php
+++ views/Person/updatefrom.php
@@ -1,1 +1,18 @@
+<ceter><form method="get" action="">
+    <label>ID   <input type="number" name="PS_id"
+        value="<?php echo $Person->PS_id;?>"/></label><br>
 
+    <label>Name   <input type="text" name="PS_name"
+    value="<?php echo $Person->PS_name;?>"/></label><br>
+
+    <label>Address  <input type="text" name="PS_add"
+    value="<?php echo $Person->PS_add;?>"/></label><br>
+
+
+<input type="hidden"name="controller"value="Person"/>
+<input type="hidden" name="oldid" value="<?php echo $Person->PS_id; ?>"/>
+<button type= "submit"name="action"value="index">Back</button>
+<button type= "submit"name="action"value="update">Update</button>
+
+
+</form>
