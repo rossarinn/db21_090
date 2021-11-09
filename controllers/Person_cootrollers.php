60c04090a5d53446c79ca6a8b624e5d803a63fb2
rossarinn <rossarin.n@ku.th>
2021-11-09 18:23:30 +0700

    Update Person_cootrollers.php

--- controllers/Person_cootrollers.php
+++ controllers/Person_cootrollers.php
@@ -1,9 +1,9 @@
-<?php class detailOrderController{
+<?php class PersonController{
     public function index()
     {
       
-        $detailOrder_List=detailOrder::getAll();
-        require_once("./views/detailOrder/index_detailOrder.php");
+        $Person_List=Person::getAll();
+        require_once("./views/Person/index_Person.php");
     }
     public function newdetailOrder(){
         
@@ -14,35 +14,26 @@
         $orderList = Order::getAll();
         require_once('./views/detailOrder/newdetailOrder.php');
     }
-    public function adddetailOrder()
+    public function addPerson()
     {
         
-        $no_id = $_GET['no_id'];
-        $date = $_GET['date'];
-        $con_payment = $_GET['con_payment']; 
-        $เงื่อนไข = $_GET['เงื่อนไข'];
-        $stock_id1 = $_GET['stock_id1'];
-        $s_id = $_GET['s_id'];
-        $c_id = $_GET['c_id'];
-        $p_id = $_GET['p_id'];
-        $cp_color = $_GET['cp_color'];
-        $n_pจำนวน = $_GET['n_pจำนวน'];
-        $n_pscreen = $_GET['n_pscreen'];
-        $id_order = $_GET['id_order'];
+        $PS_id = $_GET['PS_id'];
+        $PS_name = $_GET['PS_name'];
+        $PS_add = $_GET['PS_add']; 
+        
 
-
-        detailOrder::Add($no_id,$date,$con_payment,$เงื่อนไข,$stock_id1,$s_id,$c_id,$p_id,$cp_color,$n_pจำนวน,$n_pscreen,$id_order);
-        detailOrderController::index();
+        Person::Add($PS_id,$PS_name,$PS_add);
+        PersonController::index();
     }
     public function search()
     {
         $key = $_GET['key'];
-        $detailOrder_List = detailOrder::search($key);
-        require_once("./views/detailOrder/index_detailOrder.php");
+        $Person_List = Person::search($key);
+        require_once("./views/Person/index_Person.php");
     }
-    public function updateForm()
+    public function updateFrom()
     {
-        $id = $_GET['no_id'];
+        $id = $_GET['PS_id'];
         $detailOrder = detailOrder::get($id);
         $customerList = customer::getAll();
         $staffList = staff::getAll();
@@ -50,38 +41,29 @@
         $productList = product::getAll();
         $orderList = Order::getAll();
         
-        require_once("./views/detailOrder/updateForm.php");
+        require_once("./views/Person/updatefrom.php");
     }
     public function update()
     {
-        $no_id = $_GET['no_id'];
-        $date = $_GET['date'];
-        $con_payment = $_GET['con_payment']; 
-        $เงื่อนไข = $_GET['เงื่อนไข'];
-        $stock_id1 = $_GET['stock_id1'];
-        $s_id = $_GET['s_id'];
-        $c_id = $_GET['c_id'];
-        $p_id = $_GET['p_id'];
-       
-        $cp_color = $_GET['cp_color'];
-        $n_pจำนวน = $_GET['n_pจำนวน'];
-        $n_pscreen = $_GET['n_pscreen'];
-        $id_order = $_GET['id_order'];
-        $oldid = $_GET['oldid'];
-        detailOrder::update($no_id,$date,$con_payment,$เงื่อนไข,$stock_id1,$s_id,$c_id,$p_id,$cp_color,$n_pจำนวน,$n_pscreen,$id_order,$oldid);
-        detailOrderController::index();
+        $PS_id = $_GET['PS_id'];
+        $PS_name = $_GET['PS_name'];
+        $PS_add = $_GET['PS_add']; 
+        
+
+        Person::Add($PS_id,$PS_name,$PS_add);
+        PersonController::index();
         
     }
-    public function deleteConFirm()
+    public function deleteConFrom()
     {
-        $id = $_GET['no_id'];
+        $id = $_GET['PS_id'];
         $detailOrder = detailOrder::get($id);
         $customerList = customer::getAll();
         $staffList = staff::getAll();
         $colorproductList = colorproduct::getAll();
         $productList = product::getAll();
         $orderList = Order::getAll();
-        require_once("./views/detailOrder/deleteConFirm.php");
+        require_once("./views/Person/deleteConfrom.php");
     }
     public function delete()
 
