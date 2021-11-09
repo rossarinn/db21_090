a144bf847fa5aa619a173e44c50cb43e7d17136c
rossarinn <rossarin.n@ku.th>
2021-11-09 17:41:06 +0700

    Update home.php

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,1 +1,12 @@
 
+<p><center><font size = "5"> Welcome to our homepage </font></center><br>
+<center><img src="https://i0.wp.com/www.beinspirational.co.uk/wp-content/uploads/2014/10/Welcome-Banner.jpg?zoom=2"  ></center>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+ฺํฺ<center><font size = "5">BY  </font></center><br>
+<center><font size = "5">รสริน เณรแตง 6220504763 </font></center>
+</p>
