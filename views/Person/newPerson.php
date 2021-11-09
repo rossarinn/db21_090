528a3870891713d583ac9adf7e6cbbb476cbe839
rossarinn <rossarin.n@ku.th>
2021-11-10 00:41:21 +0700

    Update newPerson.php

--- views/Person/newPerson.php
+++ views/Person/newPerson.php
@@ -1,1 +1,13 @@
+<form method = "GET" action = "">
+<label>ID   <input type="number" name="PS_id"/></label><br>
+<label>Name  <input type="text" name="PS_name"/></label><br>
+<label>Address   <input type="text" name="PS_add"/></label><br>
+
+
+    <input type="hidden" name="controller" value="Person"/><br>
+    <button type="submit" name="action" value="index">Back</button>
+    <button type="submit" name="action" value="addPerson">Save</button>
+</form>
+
+
 
