8483101468a827ad256eb7c80120255d6e714687
rossarinn <rossarin.n@ku.th>
2021-11-11 00:35:34 +0700

    Update updatefrom.php

--- views/Queue/updatefrom.php
+++ views/Queue/updatefrom.php
@@ -1,16 +1,19 @@
 <ceter><form method="get" action="">
-<center><label>ID   <input type="number" name="PS_id"
-        value="<?php echo $PersonList->PS_id;?>"/></label><br><center>
+<center><label>ID   <input type="text" name="Q_id"
+        value="<?php echo $QueueList->Q_id;?>"/></label><br><center>
 
-        <center> <label>Name   <input type="text" name="PS_name"
-    value="<?php echo $PersonList->PS_name;?>"/></label><br><center>
+        <center><label>TIME   <input type="number" name="Q_time"
+        value="<?php echo $QueueList->Q_time;?>"/></label><br><center>
 
-    <center> <label>Address  <input type="text" name="PS_add"
-    value="<?php echo $PersonList->PS_add;?>"/></label><br><center>
+        <center><label>PERSON ID   <input type="text" name="PS_id"
+        value="<?php echo $QueueList->PS_id;?>"/></label><br><center>
 
+        <center><label>SDP ID   <input type="text" name="SDP_ID"
+        value="<?php echo $QueueList->SDP_ID;?>"/></label><br><center>
 
-<input type="hidden"name="controller"value="Person"/>
-<input type="hidden" name="oldid" value="<?php echo $Person->PS_id; ?>"/>
+        
+<input type="hidden"name="controller"value="Queue"/>
+<input type="hidden" name="oldid" value="<?php echo $Queue->Q_id; ?>"/>
 <center><button type= "submit"name="action"value="index">Back</button>
 <button type= "submit"name="action"value="update">Update</button><center>
 
