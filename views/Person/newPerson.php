9cb891a4330ca919890fac11f04572ee493eb1fd
rossarinn <rossarin.n@ku.th>
2021-11-10 14:09:16 +0700

    Update newPerson.php

--- views/Person/newPerson.php
+++ views/Person/newPerson.php
@@ -1,7 +1,8 @@
 <form method = "GET" action = "">
-<label>ID   <input type="number" name="PS_id"/></label><br>
-<label>Name  <input type="text" name="PS_name"/></label><br>
-<label>Address   <input type="text" name="PS_add"/></label><br>
+
+    <label>ID   <input type="number" name="PS_id"/></label><br>
+    <label>Name  <input type="text" name="PS_name"/></label><br>
+    <label>Address   <input type="text" name="PS_add"/></label><br>
 
 
     <input type="hidden" name="controller" value="Person"/><br>
