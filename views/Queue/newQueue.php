c52cc63084b153d343a7a88993e3276262708686
rossarinn <rossarin.n@ku.th>
2021-11-10 21:12:42 +0700

    Create newQueue.php

---
+++ views/Queue/newQueue.php
@@ -0,0 +1,14 @@
+<form method = "GET" action = "">
+
+<center><label>ID   <input type="text" name="Q_id"/></label><br><center>
+<center><label>TIME  <input type="number" name="Q_time"/></label><br><center>
+<center><label>PERSON ID   <input type="text" name="PS_id"/></label><br><center>
+<center><label>SDP ID   <input type="text" name="SDP_ID"/></label><br><center>
+
+    <input type="hidden" name="controller" value="Queue"/><br>
+    <center><button type="submit" name="action" value="index">Back</button>
+    <button type="submit" name="action" value="addQueue">Save</button><center>
+</form>
+
+
+
