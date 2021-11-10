3e95beaa4ef2874585bb9e3209eebc339ab59a13
rossarinn <rossarin.n@ku.th>
2021-11-11 01:26:29 +0700

    Update updatefrom.php

--- views/Queue/updatefrom.php
+++ views/Queue/updatefrom.php
@@ -1,5 +1,6 @@
 <ceter><form method="get" action="">
-<center><label>ID   <input type="text" name="Q_id"
+
+        <center><label>ID   <input type="text" name="Q_id"
         value="<?php echo $QueueList->Q_id;?>"/></label><br><center>
 
         <center><label>TIME   <input type="number" name="Q_time"
@@ -11,11 +12,11 @@
         <center><label>SDP ID   <input type="text" name="SDP_ID"
         value="<?php echo $QueueList->SDP_ID;?>"/></label><br><center>
 
-        
-<input type="hidden"name="controller"value="Queue"/>
+        <input type="hidden"name="controller"value="Queue"/>
 <input type="hidden" name="oldid" value="<?php echo $Queue->Q_id; ?>"/>
 <center><button type= "submit"name="action"value="index">Back</button>
 <button type= "submit"name="action"value="update">Update</button><center>
+
 
 
 </form>
