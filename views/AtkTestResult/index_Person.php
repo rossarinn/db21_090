6c5c5c3cc964ffafbdee7af4504e36c722f1adae
rossarinn <rossarin.n@ku.th>
2021-11-10 15:51:23 +0700

    Create index_Person.php

---
+++ views/AtkTestResult/index_Person.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+    border-collapse: collapse;
+    border : none;
+    width: 70%;
+}
+
+th, td {
+    text-align: center;
+    padding: 8px;
+
+}
+
+tr:nth-child(even){background-color: #D3D3D3}
+tr:hover {background-color: #FFFFFF}
+th {
+    background-color: #808080;
+    color: white;
+}
+</style>
+
+<style>
+.button {
+    background-color: #808080;
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+}
+</style>
+</head>
+<body bgcolor = "#FFFFF0"><center>
+
+<h2><center>PERSON</center></h2>
+
+
+<table border=1 >
+
+
+<form method="get" action="">
+        <input type="text" name="key" size = "30">
+        <input type="hidden" name="controller" value="Person"  />
+        <button type="submit" name="action" value="search"> Search </button>
+    </form>
+    
+
+    <br><tr>
+        <th>ID </th>
+        <th>NAME</th>
+        <th>ADDRESS</th>
+        <th>UPDATE</th>
+        <th>DELETE</th>
+        
+<?php foreach($PersonList as $Person)
+{
+        echo "<tr><td>$Person->PS_id</td>
+        <td>$Person->PS_name</td>
+        <td>$Person->PS_add</td>
+        
+       
+        <td><a href=?controller=Person&action=updateFrom&PS_id=$Person->PS_id>Update</a></td>
+        <td><a href=?controller=Person&action=deleteConfrom&PS_id=$Person->PS_id>Delete</a></td>
+        
+        </tr>";
+
+}
+
+echo "</table>";
+?>
+<br>
+</center>
+
+<center><a class="button" href="?controller=Person&action=newPerson">Addnew</a></center>
+
+
+
+</body>
+<br>
+<br>
+
+</html>
+
