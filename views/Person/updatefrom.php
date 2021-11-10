12f1aa6c41b05362a32b416abc0ea3c43761f1e1
rossarinn <rossarin.n@ku.th>
2021-11-10 15:43:39 +0700

    Update updatefrom.php

--- views/Person/updatefrom.php
+++ views/Person/updatefrom.php
@@ -1,18 +1,18 @@
 <ceter><form method="get" action="">
-    <label>ID   <input type="number" name="PS_id"
-        value="<?php echo $PersonList->PS_id;?>"/></label><br>
+<center><label>ID   <input type="number" name="PS_id"
+        value="<?php echo $PersonList->PS_id;?>"/></label><br><center>
 
-    <label>Name   <input type="text" name="PS_name"
-    value="<?php echo $PersonList->PS_name;?>"/></label><br>
+        <center> <label>Name   <input type="text" name="PS_name"
+    value="<?php echo $PersonList->PS_name;?>"/></label><br><center>
 
-    <label>Address  <input type="text" name="PS_add"
-    value="<?php echo $PersonList->PS_add;?>"/></label><br>
+    <center> <label>Address  <input type="text" name="PS_add"
+    value="<?php echo $PersonList->PS_add;?>"/></label><br><center>
 
 
 <input type="hidden"name="controller"value="Person"/>
 <input type="hidden" name="oldid" value="<?php echo $Person->PS_id; ?>"/>
-<button type= "submit"name="action"value="index">Back</button>
-<button type= "submit"name="action"value="update">Update</button>
+<center><button type= "submit"name="action"value="index">Back</button>
+<button type= "submit"name="action"value="update">Update</button><center>
 
 
 </form>
