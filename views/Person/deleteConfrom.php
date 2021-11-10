8fc27b6c895ccc8d0aec527f56aa54ea69be2517
rossarinn <rossarin.n@ku.th>
2021-11-10 15:43:45 +0700

    Update deleteConfrom.php

--- views/Person/deleteConfrom.php
+++ views/Person/deleteConfrom.php
@@ -1,6 +1,6 @@
-<?php echo "<br> คุณแน่ใจนะคะว่าคุณจะลบ? (>*<) <br>
+<center><?php echo "<br> คุณแน่ใจแล้วใช่ไหม ว่าจะลบ? (>*<) <br>
 
-<br> $PersonList->PS_id $PersonList->PS_name <br>"; ?>
+<br> $PersonList->PS_id $PersonList->PS_name <br><br />"; ?><center>
 
 
 
@@ -10,11 +10,11 @@
 
     <input type="hidden" name="controller" value="Person" />
 
-    <input type="hidden" name="PS_id" value="<?php echo $PersonList->PS_id; ?>" />
+    <input type="hidden" name="PS_id" value="<?php echo $PersonList->PS_id; ?><br />" />
 
-    <button type="submit" name="action" value="index">back</button>
+    <center>  <button type="submit" name="action" value="index">back</button>
 
-    <button type="submit" name="action" value="delete">delete</button>
+    <button type="submit" name="action" value="delete">delete</button><center>
 
     
 
