738d3b642c0ecabc1d5d88064fc239daacd2b3c4
rossarinn <rossarin.n@ku.th>
2021-11-10 17:18:02 +0700

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,19 +1,26 @@
 <?php
 $controllers = array('pages'=>['home','error'],
-                      'Person'=>['index','newPerson','search','addPerson','delete','deleteConfrom','updateFrom','update']);
+                      'Person'=>['index','newPerson','search','addPerson','delete','deleteConfrom','updateFrom','update'],
+                      'Atk'=>['index','newAtk','search','addAtk','delete','deleteConfrom','updateFrom','update']);
 
 function call($controller,$action){
     require_once("./controllers/".$controller."_controller.php");
     
     switch($controller)
     {
-        case "pages":    $controller = new PagesController();
+        case "pages":   $controller = new PagesController();
                         break;
 
         case "Person":  $controller = new PersonController();
                         require_once("./models/Person.php");
                         
                         break;
+
+        case "Atk":     $controller = new AtkController();
+                        require_once("./models/Atk.php");
+                        
+                        break;
+
     }
     $controller->{$action}();
 
