32016cdd034558ba8cbac3825aa30bff88b13406
rossarinn <rossarin.n@ku.th>
2021-11-10 15:51:17 +0700

    Create deleteConfrom.php

---
+++ views/AtkTestResult/deleteConfrom.php
@@ -0,0 +1,25 @@
+<center><?php echo "<br> คุณแน่ใจแล้วใช่ไหม ว่าจะลบ? (>*<) <br>
+
+<br> $PersonList->PS_id $PersonList->PS_name <br><br />"; ?><center>
+
+
+
+<form method="get" action="">
+
+
+
+    <input type="hidden" name="controller" value="Person" />
+
+    <input type="hidden" name="PS_id" value="<?php echo $PersonList->PS_id; ?><br />" />
+
+    <center>  <button type="submit" name="action" value="index">back</button>
+
+    <button type="submit" name="action" value="delete">delete</button><center>
+
+    
+
+
+
+</form>
+
+
