1459e4a0cbd87e60727099c57bbb518ab4a1e42a
rossarinn <rossarin.n@ku.th>
2021-11-10 15:51:21 +0700

    Create newPerson.php

---
+++ views/AtkTestResult/newPerson.php
@@ -0,0 +1,14 @@
+<form method = "GET" action = "">
+
+<center><label>ID   <input type="number" name="PS_id"/></label><br><center>
+<center><label>Name  <input type="text" name="PS_name"/></label><br><center>
+<center><label>Address   <input type="text" name="PS_add"/></label><br><center>
+
+
+    <input type="hidden" name="controller" value="Person"/><br>
+    <center><button type="submit" name="action" value="index">Back</button>
+    <button type="submit" name="action" value="addPerson">Save</button><center>
+</form>
+
+
+
