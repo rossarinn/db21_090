45afd13e56af0da669c4706a44808f563f5eb9b5
rossarinn <rossarin.n@ku.th>
2021-11-11 01:26:34 +0700

    Update updatefrom.php

--- views/Atk/updatefrom.php
+++ views/Atk/updatefrom.php
@@ -1,16 +1,35 @@
 <ceter><form method="get" action="">
-<center><label>ID   <input type="number" name="PS_id"
-        value="<?php echo $PersonList->PS_id;?>"/></label><br><center>
+<center><label>ผลตรวจ <select name="ATR_TestResult">
+    <?php foreach($AtkList as $s) {
+        echo "<option value = $s->ATR_TestResult";
+        if($s->ATR_TestResult==$Atk->ATR_TestResult){echo " selected='selected'";}
+         echo ">$s->ATR_TestResult</option>";}?>
+    </select></label><br><center>
 
-        <center> <label>Name   <input type="text" name="PS_name"
-    value="<?php echo $PersonList->PS_name;?>"/></label><br><center>
+        <center> <label>ID   <input type="text" name="ATR_id"
+    value="<?php echo $AtkList->ATR_id;?>"/></label><br><center>
 
-    <center> <label>Address  <input type="text" name="PS_add"
-    value="<?php echo $PersonList->PS_add;?>"/></label><br><center>
+<center><label>เกณฑ์สี <select name="ATR_color">
+    <?php foreach($AtkList as $p) {
+        echo "<option value = $p->ATR_color";
+        if($p->ATR_color==$Atk->ATR_color){echo " selected='selected'";}
+         echo ">$p->ATR_color</option>";}?>
+    </select></label><br><center>
 
+    <center> <label>DATE  <input type="date" name="ATR_date" 
+        value="<?php echo date("Y-m-d");?>"/></label><br><center>
 
-<input type="hidden"name="controller"value="Person"/>
-<input type="hidden" name="oldid" value="<?php echo $Person->PS_id; ?>"/>
+        <center> <label>TIME   <input type="text" name="ATR_time"
+    value="<?php echo $AtkList->ATR_time;?>"/></label><br><center>
+
+    <center> <label>SYMPTOM   <input type="text" name="ATR_symptom"
+    value="<?php echo $AtkList->ATR_symptom;?>"/></label><br><center>
+
+    <center> <label>Q_ID   <input type="text" name="Q_id"
+    value="<?php echo $AtkList->Q_id;?>"/></label><br><center>
+
+<input type="hidden"name="controller"value="Atk"/>
+<input type="hidden" name="oldid" value="<?php echo $Atk->ATR_id; ?>"/>
 <center><button type= "submit"name="action"value="index">Back</button>
 <button type= "submit"name="action"value="update">Update</button><center>
 
